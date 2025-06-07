58c576c37e51c3797f456d6fb4068002856b8fb7
Valeria Esquivel <esq22001@byui.edu>
2025-06-07 03:15:58 -0600

    Codes for validation

--- utilities/ward-validator.js
+++ utilities/ward-validator.js
@@ -46,7 +46,7 @@ validate.wardRules = () => {
 validate.validateWard = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
+    return res.status(400).json({
       errors: errors.array().map((error) => ({
         field: error.param,
         message: error.msg,
